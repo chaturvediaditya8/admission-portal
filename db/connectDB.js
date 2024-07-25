3df5bc58a67819618939c9210c8a9ccffd88aaaf
chaturvediaditya8 <chaturvediaditya8@gmail.com>
2024-07-25 22:53:46 +0530

    25-07-2024

--- db/connectDB.js
+++ db/connectDB.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
-const Local_URL ='mongodb://127.0.0.1:27017/admissionPortal'
+// const Local_URL ='mongodb://127.0.0.1:27017/admissionPortal'
+const Live_URL = "mongodb+srv://chaturvediaditya8:Program1ngAP1@ap1express.1qq4hie.mongodb.net/admissionPortal?retryWrites=true&w=majority&appName=admissionPortal"
+
 const connectDB =()=>{
-    return mongoose.connect(Local_URL)
+    return mongoose.connect(Live_URL)
     .then(()=>{
         console.log("connect successfully")
     }).catch((error)=>{
